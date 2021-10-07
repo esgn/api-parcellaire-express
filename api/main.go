74495ea395a5fed4741e4ae98645caa473c41c35
David CLAIN <dclain@gmail.com>
2021-10-07 19:17:06 +0200

    Add .env management for local start

--- api/main.go
+++ api/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "os"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	flagUseEnv := flag.String("env", "", "Provides the path to .env files")
+
+	flag.Parse()
+
+	if *flagUseEnv != "" {
+		fmt.Printf("Loading .env : %v", flagUseEnv)
+		err := godotenv.Load(*flagUseEnv)
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+		fmt.Println("✅")
+	}
+
 	a := App{}
 	a.Initialize(
 		os.Getenv("POSTGRES_USER"),
