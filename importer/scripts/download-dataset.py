827876db99cd2387ec386c7f9a0b530c0def1912
David CLAIN <dclain@gmail.com>
2021-11-09 18:26:09 +0100

    progress bar

--- importer/scripts/download-dataset.py
+++ importer/scripts/download-dataset.py
@@ -6,6 +6,7 @@ import sys
 import re
 import shutil
 import requests
+from tqdm import tqdm
 from bs4 import BeautifulSoup
 from multiprocessing.pool import ThreadPool
 
@@ -22,12 +23,16 @@ def download_url(url):
     try:
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
+            _file_size = int(r.headers.get('Content-Length'))
             with open(out_file, 'wb') as f:
-                for data in r.iter_content(chunk_size=8192):
-                    f.write(data)
+                with tqdm(total=_file_size, unit='B', unit_scale=True, unit_divisor=1024) as _bar:
+                    for data in r.iter_content(chunk_size=8192):
+                        f.write(data)
+                        _bar.update(len(data))
+
     except requests.exceptions.HTTPError as err:
-        print(filename + " teléchargement échoué")
-        print(err)
+        tqdm.write(filename + " teléchargement échoué")
+        tqdm.write(err)
         sys.exit(0)
     return filename + " téléchargement réussi"
 
@@ -49,7 +54,7 @@ if __name__ == "__main__":
         if int(os.environ['MAX_PARALLEL_DL'])!=0:
             max_parallel_dl = int(os.environ['MAX_PARALLEL_DL'])
         else:
-            print("🚧 new value of MAX_PARALLEL_DL is not in integer. Ignored.", file=sys.stderr)
+            print("🚧 new value of MAX_PARALLEL_DL is not in integer or not a positive value. Ignored.", file=sys.stderr)
 
     # Extraction des liens de téléchargement
     all_links = extract_all_links(idx_url)
@@ -62,7 +67,7 @@ if __name__ == "__main__":
             testing=True
             all_links = all_links[:1]
         else:
-            print("🚧 new value of TEST_IMPORTER is not in integer. Ignored.", file=sys.stderr)
+            print("🚧 new value of TEST_IMPORTER is not in integer or not a positive value. Ignored.", file=sys.stderr)
 
     # Réinitialisation du dossier de téléchargement
     if os.path.exists(out_dir):
@@ -77,4 +82,4 @@ if __name__ == "__main__":
         print("🟣 EXECUTION EN MODE TEST => UNE SEULE ARCHIVE SERA TELECHARGEE")
     results = ThreadPool(max_parallel_dl).imap_unordered(download_url, all_links)
     for r in results:
-        print(r)
+        tqdm.write(r)
